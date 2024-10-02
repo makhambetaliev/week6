5265dcd21dc08970dbc1786163bbbe9815c4ab1a
makhambetaliev <r.maxambetaliev@newuu.uz>
2024-10-02 12:23:40 +0500

    Create week5.cpp

---
+++ week5.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    // double a;
+    // int s;
+    // do {
+    //     cin>>a;
+    // } while (a<=-58 or a>=41);
+    // do {
+    //     cin>>s;
+    // } while (s<=2);
+    // cout<<"The wind chill index is: "<<35.74+0.6215*a-35.75*pow(s,0.16)+0.4275*a*pow(s,0.16)<<"\n";
+    // return 0;
+
+
+    // int a,b,c;
+    // cin>>a>>b>>c;
+    // if (c%a==0 && c%b==0) {
+    //     cout<<"YES"<<endl;
+    // } else {
+    //     cout<<"NO"<<endl;
+    // }
+
+    // int a;
+    // cin>>a;
+    // int b,c,d;
+    // a=a-2;
+    // b=0;
+    // c=1;
+    // d=0;
+    // cout<<b<<","<<c;
+    // for(int i=0;i<=a;i++) {
+    //     d=c;
+    //     c=b+c;
+    //     b=d;
+    //     cout<<","<<c;
+    // }
+    // cout<<endl;
+
+
+    //Encrypt
+    // int a;
+    // cin>>a;
+    // int b,c,d,e;
+    // int b2,c2,d2,e2;
+    // b=a/1000;
+    // c=(a%1000)/100;
+    // d=(a%100)/10;
+    // e=a%10;
+    // cout<<b<<" "<<c<<" "<<d<<" "<<e<<endl;
+    // b2 = (b+7)%10;
+    // c2 = (c+7)%10;
+    // d2 = (d+7)%10;
+    // e2 = (e+7)%10;
+    // cout<<d2<<" "<<e2<<" "<<b2<<" "<<c2<<endl;
+
+    //Decrypt
+    // int a;
+    // cin>>a;
+    // int b,c,d,e;
+    // int b2,c2,d2,e2;
+    // b=a/1000;
+    // c=(a%1000)/100;
+    // d=(a%100)/10;
+    // e=a%10;
+    //
+    // b2=b+3;
+    // c2=c+3;
+    // d2=d+3;
+    // e2=e+3;
+    // cout<<d2<<" "<<e2<<" "<<b2<<" "<<c2<<endl;
+
+    
+}
